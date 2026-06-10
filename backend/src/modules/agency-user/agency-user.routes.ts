0a2adb49a81b9ca0fa30e7406e9ddc031c2aa904
kartheek6999 <kartheeknaidu2020@gmail.com>
2026-06-10 17:25:40 +0530

    Authentication and RBAC completed

--- backend/src/modules/agency-user/agency-user.routes.ts
+++ backend/src/modules/agency-user/agency-user.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { AgencyUserController } from "./agency-user.controller";
-
+import { requireRole } from "../auth/role.middleware";
 const router = Router();
 const agencyUserController = new AgencyUserController();
 
-router.post("/", (req, res) => agencyUserController.createAgencyUser(req, res));
+router.post(
+  "/",
+  requireRole(["OWNER"]),
+  (req, res) => agencyUserController.createAgencyUser(req, res)
+);
 
 export { router as agencyUserRoutes };
